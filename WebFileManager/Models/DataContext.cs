e5fc899c9d0ab3b3eab0afdaa555c6004a9d3faf
Kirill <kiw197204@gmail.com>
2022-11-06 17:14:03 +0300

    Добавьте файлы проекта.

---
+++ WebFileManager/Models/DataContext.cs
@@ -0,0 +1,43 @@
+﻿using MySqlConnector;
+
+namespace WebFileManager.Models
+{
+    public class DataContext
+    {
+        public string ConnectionString { get; set; }
+
+        public DataContext(string connectionString)
+        {
+            this.ConnectionString = connectionString;
+        }
+
+        private MySqlConnection GetConnection()
+        {
+            return new MySqlConnection(ConnectionString);
+        }
+
+        public List<FileModel> GetAllFileNames()
+        {
+            List<FileModel> list = new List<FileModel>();
+
+            using (MySqlConnection conn = GetConnection())
+            {
+                conn.Open();
+                MySqlCommand cmd = new MySqlCommand("SELECT * FROM webtest.files", conn);
+                using (MySqlDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        list.Add(new FileModel()
+                        {
+                            Filename = reader.GetString("filename"),
+                        });
+                    }
+                }
+            }
+
+            return list;
+        }
+        
+    }
+}
---
+++ WebFileManager/Models/FileModel.cs
@@ -0,0 +1,7 @@
+﻿namespace WebFileManager.Models
+{
+    public class FileModel
+    {
+        public string Filename { set; get; }
+    }
+}
